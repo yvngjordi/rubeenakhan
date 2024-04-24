36470ea665ba59aa892c893680acf9ca98f41d12
yvngjordi <61796788+yvngjordi@users.noreply.github.com>
2024-04-24 12:48:09 -0700

    x

--- pages/policies.tsx
+++ pages/policies.tsx
@@ -32,8 +32,8 @@ const dropdownOptions = [
 ];
 
 const imageSrc = 'bg3.jpg';
-const fetchURL = 'https://webdragon.dev/contact.php';
-const recaptchaSiteKey = '6LeJAGgpAAAAAJMPwrZeB3H5reXW_CdvDJsrMmfg';
+const fetchURL = 'https://webdragon.dev/rubeena.php';
+const recaptchaSiteKey = '6Lfi0cQpAAAAADBMtv-9-m4pTm8RBeF0pD-5JHg2';
 
 export default function HomePage() {
   const isMobile = useMediaQuery('(max-width: 768px)');
